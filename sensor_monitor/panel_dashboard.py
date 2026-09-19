5ab2f892b01eb38ec18bbd72fe8cf48c3a629840
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2026-09-19 20:30:01 +0100

    Panel update
    Signed-off-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>

--- sensor_monitor/panel_dashboard.py
+++ sensor_monitor/panel_dashboard.py
@@ -2,19 +2,25 @@ import pandas as pd
 import sqlite3
 from pathlib import Path
 import panel as pn
-from bokeh.plotting import figure,curdoc
+from bokeh.themes._carbon import json
+from bokeh.plotting import figure, curdoc
 from bokeh.models import LinearAxis, Range1d
+import numpy as np
 
-# panel serve --autoreload --address 0.0.0.0 --port 8000 --allow-websocket-origin=0.0.0.0:8000 panel_dashboard.py
+# panel serve --autoreload --address 0.0.0.0 --port 8000 --allow-websocket-origin=*:8000 panel_dashboard.py
 
+# tweaked carbon theme
+json["attrs"]["figure"]["background_fill_color"] = "#00000000"
+json["attrs"]["figure"]["border_fill_color"] = "#00000000"
 curdoc().theme = "carbon"
+
 pn.extension(design="fast", theme="dark", sizing_mode="stretch_width")
 
 DAYS_S = 60 * 60 * 24  # how many seconds in a day
 NUM_DAYS = 2  # how many days of data to read
 FREQ = "15min"  # frequency to group data items by
 
-COLORS = ["#636EFA", "#EF553B", "#00CC96", "#AB63FA", "#FFA15A", "#19D3F3", "#FF6692"]
+COLORS = ["#636EFA", "#EF553B", "#00CC96", "#AB63FA", "#FFA15A", "#19D3F3", "#FF6692", "#B6E880"]
 
 db = sorted(Path(".").glob("*.sqlite"))[-1]  # most recent database
 
@@ -26,25 +32,25 @@ df.set_index("date", inplace=True)
 df.sort_index(inplace=True)
 df_sel = df.groupby(pd.Grouper(freq=FREQ)).mean().dropna(how="all")
 
-fig_conf = dict(height=400, x_axis_type="datetime", tools="reset,xpan,xbox_zoom", sizing_mode="stretch_width")
+fig_conf = dict(height=500, x_axis_type="datetime", tools="reset,xpan,xbox_zoom", sizing_mode="stretch_width")
 
 trends = []
 for c, col in zip(["temperature", "humidity", "pressure", "iaq"], COLORS):
     data = {"x": df_sel.index, "y": df_sel[c].to_numpy()}
-    n = pn.indicators.Trend(label=c.title(), data=data, layout="row", height=100, plot_type="area", plot_color=col)
+    n = pn.indicators.Trend(label=c.title(), data=data, layout="row", height=100, plot_type="step", plot_color=col)
     trends.append(n)
 
 gasfig = figure(**fig_conf)
 gasfig.yaxis.visible = False
 gasfig.toolbar.logo = None
 
-for g, color in zip(["oxidising", "reducing", "nh3"], COLORS[len(trends) :]):
+for g, color in zip(["oxidising", "reducing", "nh3", "gas_resistance"], COLORS[len(trends) :]):
     yg = df_sel[g]
     gasfig.extra_y_ranges[g] = Range1d(yg.min(), yg.max())
     ax = LinearAxis(y_range_name=g, axis_label=g.replace("_", " ").title())
     ax.axis_label_text_color = color
     gasfig.add_layout(ax, "left")
-    gasfig.line(yg.index, yg, color=color)
+    gasfig.line(yg.index, yg, y_range_name=g, color=color)
 
 rgbfig = figure(x_range=gasfig.x_range, y_axis_label="RGBC Values", **fig_conf)
 rgbfig.xaxis.visible = False
